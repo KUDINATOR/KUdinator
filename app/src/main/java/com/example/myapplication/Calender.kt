8f8e81556a14c5e44c5ddeb3ce62d5955e78b56b
jwkim <kjw5841@naver.com>
2022-05-13 23:58:23 +0900

    update

---
+++ app/src/main/java/com/example/myapplication/Calender.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication
+
+data class Calender(
+    var what : String? = null,
+    var when_time : String? = null
+)
---
+++ app/src/main/java/com/example/myapplication/StyleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_cloth.*
+
+class StyleActivity : AppCompatActivity(){
+
+}
